1a6cdb2cf2a44d4dd3525aeab5d689a6a6c5f991
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-24 10:50:22 -0500

    Actualizar bot-grupos.js

--- plugins/bot-grupos.js
+++ plugins/bot-grupos.js
@@ -4,9 +4,9 @@ let colab = `✨️ ${namechannel}
 ${channel}`
 m.react('🤍') 
 
-await conn.sendFile(m.chat, pp, 'ofc.jpg', colab, estilo)}
+//await conn.sendFile(m.chat, pp, 'ofc.jpg', colab, estilo)}
 
-//await conn.sendFile(m.chat, pp, 'thumbnail.jpg', colab.trim(), m, null, estilo)}
+await conn.sendFile(m.chat, pp, 'thumbnail.jpg', colab.trim(), wm, packname, m, null, rcanal)}
 
 handler.command = /^grupos|linkyl|gruposofc|gruposoficiales$/i
 handler.register = true
